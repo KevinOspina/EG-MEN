b7934a1415e966e3f2c06f4d173530ca7b90ac9e
mcMEJIA <mejiasebastian1499@gmail.com>
2020-11-24 22:20:03 -0500

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
 from controllers.Canal import Canal
-from db.db import cnx
 
 app = Flask(__name__)
 
@@ -19,7 +18,3 @@ def post():
 def delete():
     body = request.json
     return (Canal.delete(body))
-
-if __name__ == '__main__':
- app.run(port = 3000, debug=True)
-
